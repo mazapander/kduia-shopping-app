91adb00bcbf0e28ccbb09f46dcee8408df54b4e6
mazapander <98283345+mazapander@users.noreply.github.com>
2024-02-01 09:06:56 +0100

    Update AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,40 +1,100 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { createContext, useReducer } from 'react';
 
+// 5. The reducer - this is used to update the state, based on the action
+export const AppReducer = (state, action) => {
+    let new_expenses = [];
+    switch (action.type) {
+        case 'ADD_QUANTITY':
+            let updatedqty = false;
+            state.expenses.map((expense)=>{
+                if(expense.name === action.payload.name) {
+                    expense.quantity = expense.quantity + action.payload.quantity;
+                    updatedqty = true;
+                }
+                new_expenses.push(expense);
+                return true;
+            })
+            state.expenses = new_expenses;
+            action.type = "DONE";
+            return {
+                ...state,
+            };
 
-import { AppProvider } from './context/AppContext';
-import CartValue from './components/CartValue';
-import ExpenseList from './components/ExpenseList';
-import ItemSelected from './components/ItemSelected';
-import Location from './components/Location';
+            case 'RED_QUANTITY':
+                state.expenses.map((expense)=>{
+                    if(expense.name === action.payload.name) {
+                        expense.quantity = expense.quantity - action.payload.quantity;
+                    }
+                    expense.quantity = expense.quantity < 0 ? 0: expense.quantity;
+                    new_expenses.push(expense);
+                    return true;
+                })
+                state.expenses = new_expenses;
+                action.type = "DONE";
+                return {
+                    ...state,
+                };
+        case 'DELETE_ITEM':
+            state.expenses.map((expense)=>{
+                if(expense.name === action.payload.name) {
+                    expense.quantity = 0;
+                }
+                new_expenses.push(expense);
+                return true;
+            })
+            state.expenses = new_expenses;
+            action.type = "DONE";
+            return {
+                ...state,
+            };
+    case 'CHG_LOCATION':
+            action.type = "DONE";
+            state.Location = action.payload;
+            return {
+                ...state
+            }
 
-const App = () => {
+        default:
+            return state;
+    }
+};
+
+// 1. Sets the initial state when the app loads
+const initialState = {
+    expenses: [
+        { id: "Shirt", name: 'Shirt', quantity: 0, unitprice: 500 },
+        { id: "Jeans", name: 'Jeans', quantity: 0, unitprice: 300 },
+        { id: "Dress", name: 'Dress', quantity: 0, unitprice: 400 },
+        { id: "Dinner set", name: 'Dinner set', quantity: 0, unitprice: 600 },
+        { id: "Bags", name: 'Bags', quantity: 0, unitprice: 200 },
+    ],
+    Location: '£'
+};
+
+// 2. Creates the context this is the thing our components import and use to get the state
+export const AppContext = createContext();
+
+// 3. Provider component - wraps the components we want to give access to the state
+// Accepts the children, which are the nested(wrapped) components
+export const AppProvider = (props) => {
+    // 4. Sets up the app state. takes a reducer, and an initial state
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    const totalExpenses = state.expenses.reduce((total, item) => {
+        return (total = total + (item.unitprice*item.quantity));
+    }, 0);
+state.CartValue = totalExpenses;
+
     return (
-        <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Shopping App</h1>
-                <div className='row mt-3'> 
-                    <div className='col-sm'>
-                        <CartValue />
-                    </div>
-                    <div className='col-sm'>
-                        <Location />
-                    </div>
-                </div>
-                <h3 className='mt-3'>Shopping Cart</h3>
-                <div className='row '>
-                    <div className='col-sm'>
-                        <ExpenseList />
-                    </div>
-                </div>
-                <h3 className='mt-3'>Add Items</h3>
-                <div className='row mt-3'>
-                    <div className='col-sm'>
-                        <ItemSelected/>
-                    </div>
-                </div>
-            </div>
-        </AppProvider>
+        <AppContext.Provider
+            value={{
+                expenses: state.expenses,
+                CartValue: state.CartValue,
+                dispatch,
+                Location: state.Location
+            }}
+        >
+            {props.children}
+        </AppContext.Provider>
     );
 };
-export default App;
